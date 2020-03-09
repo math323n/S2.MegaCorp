69800b0b34eea5e73215fe6b5e04b00b727f564e
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-03-09 10:26:50 +0100

    Add project files.

---
+++ S2.MegaCorp.Entities/Order.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace S2.MegaCorp.Entities
+{
+    public class Order
+    {
+        int id;
+        DateTime orderDate;
+        DateTime shipmentDate;
+
+        public Order(int id, DateTime orderDate, DateTime shipmentDate)
+        {
+            Id = id;
+            OrderDate = orderDate;
+            ShipmentDate = shipmentDate;
+
+            (bool datesAreValid, string message) datesValidationresult = ValidateDates(orderDate, shipmentDate);
+            if(!datesValidationresult.datesAreValid)
+            {
+                throw new ArgumentException(datesValidationresult.message);
+            }
+
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public DateTime OrderDate
+        {
+            get
+            {
+                return orderDate;
+            }
+
+            set
+            {
+                if(shipmentDate != default)
+                {
+                    (bool datesAreValid, string message) datesValidationresult = ValidateDates(orderDate, shipmentDate);
+                    if(!datesValidationresult.datesAreValid)
+                    {
+                        throw new InvalidOperationException(datesValidationresult.message);
+                    }
+                    orderDate = value;
+                }
+            }
+        }
+
+        public DateTime ShipmentDate
+        {
+            get
+            {
+                return shipmentDate;
+            }
+
+            set
+            {
+                if(orderDate != default)
+                {
+                    (bool datesAreValid, string message) datesValidationresult = ValidateDates(orderDate, shipmentDate);
+                    if(!datesValidationresult.datesAreValid)
+                    {
+                        throw new InvalidOperationException(datesValidationresult.message);
+                    }
+                    shipmentDate = value;
+                }
+            }
+        }
+
+        public static (bool, string) ValidateDates(DateTime orderDate, DateTime shipmentDate)
+        {
+            if(shipmentDate > orderDate)
+            {
+                return (true, String.Empty);
+            }
+            else
+            {
+                return (false, "Invalid dates");
+            }
+        }
+    }
+}
---
+++ S2.MegaCorp.Tests/OrderTests.cs
@@ -0,0 +1,56 @@
+using S2.MegaCorp.Entities;
+using System;
+using Xunit;
+
+namespace S2.MegaCorp.Tests
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void OrderDatePreceedsShipmentDate()
+        {
+            // Arrange:
+            int id = 1;
+            DateTime orderDate = new DateTime(2020, 03, 09);
+            DateTime shipmentDate = orderDate.AddDays(1);
+            Order order;
+
+            // Act:
+            order = new Order(id, orderDate, shipmentDate);
+
+            // Assert:
+            Assert.True(order.ShipmentDate > order.OrderDate);
+        }
+
+        [Fact]
+        public void ShipmentDatePreceedsOrderDateThrows()
+        {
+            // Arrange:
+            int id = 1;
+            DateTime orderDate = new DateTime(2020, 03, 09);
+            DateTime shipmentDate = orderDate.AddDays(-1);
+            Order order;
+
+            // Actsert:
+            Assert.Throws<ArgumentException>(
+                () => order = new Order(id, orderDate, shipmentDate));
+        }
+
+        [Fact]
+        public void OrderMutatesToValidState()
+        {
+            // Arrange:
+            int id = 1;
+            DateTime orderDate = new DateTime(2020, 03, 09);
+            DateTime shipmentDate = orderDate.AddDays(1);
+            Order order = new Order(id, orderDate, shipmentDate);
+            DateTime newShipmentDate = shipmentDate.AddDays(1);
+
+            // Act:
+            order.ShipmentDate = newShipmentDate;
+
+            // Assert:
+            Assert.True(order.OrderDate < order.ShipmentDate);
+        }
+    }
+}
